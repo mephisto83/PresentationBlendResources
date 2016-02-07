e1b667d9dcc89275cf0c54781ebbe82f4af0e595
Andrew Porter <mephisto83@hotmail.com>
2016-02-07 14:24:08 +0100

    lk

---
+++ render.py
@@ -0,0 +1,26 @@
+import bpy
+import sys
+argv = sys.argv
+try:
+    index = argv.index("--") + 1
+except:
+    index = len(argv)
+
+argv = argv[index:]
+start = 1
+stop = 1
+default_camera = "default_camera"
+print(argv)
+if(argv[0]):
+	start = int(argv[0])
+
+if(argv[1]):
+	stop = int(argv[1])
+
+if(argv[2]):
+	default_camera = argv[2]
+	
+bpy.context.scene.frame_start = start
+bpy.context.scene.frame_end = stop
+bpy.context.scene.camera = bpy.data.objects[default_camera]
+bpy.ops.ptcache.bake_all(bake=True)
